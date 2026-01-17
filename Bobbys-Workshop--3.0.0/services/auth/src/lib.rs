f4444bbf89afc5fc3b56e011e587c14687954826
bboy9090 <kidbuggz@yahoo.com>
2026-01-17 04:51:41 -0500

    kok

--- Bobbys-Workshop--3.0.0/services/auth/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod oidc;
-pub mod saml;
-pub mod roles;
--- Bobbys-Workshop--3.0.0/start-silent.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Start Bobby's Workshop without showing console windows
-cd /d "%~dp0"
-REM Use VBScript to run completely hidden
-cscript //nologo start-hidden.vbs
-exit
